d77dd81d26a4156f18a0387d2e5eda78ca81e80b
renitagid <renita.y.ip@gmail.com>
2022-12-27 22:14:08 -0800

    added dice roller, pig latin, and chooser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import React, { useState } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import PigLatin from "./pages/PigLatin";
+import DiceRoller from "./pages/DiceRoller";
+import NotFound from "./pages/NotFound";
+import Chooser from "./pages/Chooser";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="piglatin" element={<PigLatin />} />
+        <Route path="dice" element={<DiceRoller />} />
+        <Route path="chooser" element={<Chooser />} />
+      </Routes>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
